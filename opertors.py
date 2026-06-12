082ecbe8859e1961b425c8a09a9183be48f65576
MOHIT MALVIYA <02mohitmalviya@gmail.com>
2026-06-12 23:28:21 +0530

    Add files via upload

---
+++ opertors.py
@@ -0,0 +1,46 @@
+# Arithmetic operators 
+a = 6
+b = 8
+c = a+b
+print(c)
+
+# Assignment operators 
+a = 4-2 # Assign 4-2 in a
+print(a)
+b = 6
+b += 3 # put any + - * / 
+print(b)
+
+# Comparsion operators
+d = 5<4
+print(d)
+e = 5>4
+print(e)
+f = 5>=5
+print(f)
+g = 5<=5
+print(e)
+h = 5!=5
+print(h)
+i = 5==5
+print(i)
+
+# LogicaL operators
+
+e = True or False
+
+# Truth table of 'or'
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+# Truth table of 'and'
+print("True and False is", True and False)
+print("True and True is", True and True)
+print("False and True is", False and True)
+print("False and False is", False and False)
+
+
+# 'not' operators
+print(not(False))
